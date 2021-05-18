5c6ec8aab26bcaba9312476c54b50790f6477d8c
Jasper Anders <jasper.anders@online.de>
2021-05-18 23:15:13 +0200

    updated seo

--- src/components/globals/seo.js
+++ src/components/globals/seo.js
@@ -2,9 +2,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import SchemaOrg from "./schema-org"
 
-const SEO = ({ description, lang, meta, title }) => {
-  const { site } = useStaticQuery(
+const SEO = ({ description, image, url, isBlogPost, datePublished, title }) => {
+  let { site } = useStaticQuery(
     graphql`
       query {
         site {
@@ -12,80 +13,47 @@ const SEO = ({ description, lang, meta, title }) => {
             title
             description
             author
+            siteUrl
           }
         }
       }
     `
   )
 
-  const metaDescription = description || site.siteMetadata.description
+  site = site.siteMetadata
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang
-      }}
-      title={title}
-      titleTemplate={`%s`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription
-        },
-        {
-          property: `og:title`,
-          content: title
-        },
-        {
-          property: `og:site_name`,
-          content: title
-        },
-        {
-          property: `og:description`,
-          content: metaDescription
-        },
-        {
-          property: `og:type`,
-          content: `website`
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author
-        },
-        {
-          name: `twitter:title`,
-          content: title
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription
-        },
-        {
-          name: `msapplication-TilteColor`
-        },
-        {
-          name: `theme-color`
-        }
-      ].concat(meta)}
-    />
+    <>
+      <Helmet>
+        {/* General tags */}
+        <title>{title ? title : site.title}</title>
+        <meta
+          name="description"
+          content={description ? description : site.description}
+        />
+        {image && <meta name="image" content={image} />}
+
+        {/* OpenGraph tags */}
+        <meta property="og:url" content={url} />
+        {isBlogPost ? <meta property="og:type" content="article" /> : null}
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        {image && <meta property="og:image" content={image} />}
+      </Helmet>
+      <SchemaOrg
+        isBlogPost={isBlogPost}
+        url={url}
+        title={title}
+        image={image ? image : undefined}
+        description={description}
+        datePublished={datePublished}
+        canonicalUrl={site.siteUrl}
+        author={site.author}
+        organization={site.organization}
+        defaultTitle={site.title}
+      />
+    </>
   )
-}
-
-SEO.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  meta: PropTypes.array,
-  lang: PropTypes.string
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``
 }
 
 export default SEO
